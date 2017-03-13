c852b74b3647ad61047082085bf397e1c7f2f808
Jesse Warden <jesse@jessewarden.com>
2017-03-12 21:42:15 -0400

    testing imports

--- src/com/jessewarden/ff6/index.js
+++ src/com/jessewarden/ff6/index.js
@@ -1,12 +1,13 @@
 module.exports = {
-    battle: {
-        battleTimer: require('./battle/battletimer'),
-        battleUtils: require('./battle/battleutils'),
-        character: require('./battle/character')
-    },
-    enums: {
-        BattleState: require('./enums/BattleState'),
-        Row: require('./enums/Row')
-    },
-    relics: require('./relics')
+    // battle: {
+    //     battleTimer: require('./battle/battletimer'),
+    //     battleUtils: require('./battle/battleutils'),
+    //     character: require('./battle/character')
+    // },
+    // enums: {
+    //     BattleState: require('./enums/BattleState'),
+    //     Row: require('./enums/Row')
+    // },
+    // relics: require('./relics')
+    testing: "Man"
 };
